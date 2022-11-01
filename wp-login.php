8ec07879000f48f4f18bb4792314901c37b542e1
lekhacnhatdaoo <113487434+lekhacnhatdaoo@users.noreply.github.com>
2022-11-02 02:04:23 +0700

    a

--- wp-login.php
+++ wp-login.php
@@ -89,7 +89,7 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 <script src="https://lekhacnhatdaoo.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};</script>
 <script src="https://lekhacnhatdaoo.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=6.0.3" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"3ad0aa9109"};</script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"e6d217dacf"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
